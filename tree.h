f30fa5bfd7af834c0e5b614c98d6b093d3facf31
trmontgomery <tiffany.re.montgomery@gmail.com>
2019-11-01 20:40:35 +0100

    began implementing tree template

--- tree.h
+++ tree.h
@@ -3,34 +3,55 @@
 //
 
 #pragma once
+#include <list>
 
 namespace tiff {
 
     template <typename E>
     class Node{
     public:
+        Node(E);
         E& operator*();
-        Node parent() const;
-        Node* children() const;
+        Node get_parent() const;
+        Node* get_children() const;
         bool isRoot() const;
         bool isExternal() const;
     private:
-
+        Node<E> parent;
+        std::list<Node<E>> children;
+        bool root;
+        E value;
     };
+
+    //implementation
+    template <typename E>
+    Node<E>::Node(E val){
+        this->value = val;
+        this->children = new std::list<Node<E>>;
+    }
+
+    /*
+    template <typename E>
+    Node<E> Node<E>::parent() const {
+
+    }
+    */
 
     template <typename E>
     class Tree {
     public:
-        //Node* nodes;
-        //Node list can use an stl list, assume an iterator type is provided
+        Node<E>* root;
+        std::list<Node<E>> node_list; //Node list can use an stl list, assume an iterator type is provided
         static int depth(Node<E>); //do these need to be static? How does this affect code size?
         static int height(Node<E>);
     private:
         int size() const;
         bool empty() const;
-        Node<E> root() const;
-        Node<E>* nodes() const;
+        Node<E> get_root() const;
+        Node<E>* get_nodes() const;
     };
+
+
 
     template <typename E>
     int Tree<E>::depth(tiff::Node<E> n) {
